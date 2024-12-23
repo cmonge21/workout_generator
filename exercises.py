77896543ad8ed5375f44c8a2591aa9f57ba837cf
cmonge21 <carlypres@gmail.com>
2024-12-23 12:53:00 -0400

    exercise lists created

---
+++ exercises.py
@@ -0,0 +1,11 @@
+push_exercise = [benchpress, incline_benchpress, overhead_press, pushup, dip, tricep_pushdown, pushpress, floorpress, skullcrusher]
+
+pull_exercise = [seated_row, lat_pulldown, bentover_row, pullup, chinup, bicep_curl, face_pull, pendlay_row, upright_row]
+
+squat_exercise = [barbell_backsquat, barbell_frontsquat, goblet_squat, jumping_squat, sumo_squat, overhead_squat, singleleg_squat, box_squat]
+
+lunge_exercise = [forward_lunge, backward_lunge, curtsy_lunge, dumbbell_stepup, rfess, ffess, side_lunge, walking_lunge]
+
+carry_exercise = [farmers_carry, frontrack_carry, overhead_carry, suitcase_carry, goblet_carry, zercher_carry]
+
+hinge_exercise = [conventional_deadlift, romanian_deadlift, sumo_deadlift, singleleg_rdl, good_morning, hip_thrust]
