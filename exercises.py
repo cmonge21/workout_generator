254099df36b2b87ec146db264161966ed2179122
cmonge21 <carlypres@gmail.com>
2024-12-24 20:19:14 -0400

    created dictionary for category and exercise

--- exercises.py
+++ exercises.py
@@ -1,11 +1,39 @@
-push_exercise = [benchpress, incline_benchpress, overhead_press, pushup, dip, tricep_pushdown, pushpress, floorpress, skullcrusher]
+import random
 
-pull_exercise = [seated_row, lat_pulldown, bentover_row, pullup, chinup, bicep_curl, face_pull, pendlay_row, upright_row]
+push_exercise = ["bench press", "incline bench press", "overhead press", "pushup", "dip", "tricep pushdown"," push press", "floor press", "skull crusher"]
 
-squat_exercise = [barbell_backsquat, barbell_frontsquat, goblet_squat, jumping_squat, sumo_squat, overhead_squat, singleleg_squat, box_squat]
+pull_exercise = ["seated row", "lat pulldown", "bentover row", "pull-up", "chin-up", "bicep curl", "face pull", "pendlay row", "upright row"]
 
-lunge_exercise = [forward_lunge, backward_lunge, curtsy_lunge, dumbbell_stepup, rfess, ffess, side_lunge, walking_lunge]
+squat_exercise = ["barbell back squat", "barbell front squat", "goblet squat", "jumping squat", "sumo squat", "overhead squat", "single leg squat", "box squat"]
 
-carry_exercise = [farmers_carry, frontrack_carry, overhead_carry, suitcase_carry, goblet_carry, zercher_carry]
+lunge_exercise = ["forward lunge", "backward lunge", "curtsy lunge", "dumbbell stepup", "rearfoot elevated split squat", "front foot elevated split squat", "side lunge", "walking lunge"]
 
-hinge_exercise = [conventional_deadlift, romanian_deadlift, sumo_deadlift, singleleg_rdl, good_morning, hip_thrust]
+carry_exercise = ["farmers carry", "frontrack carry", "overhead carry", "suitcase carry", "goblet carry", "zercher carry"]
+
+hinge_exercise = ["conventional deadlift", "romanian deadlift", "sumo deadlift", "single leg romanian deadlift", "good morning", "hip thrust"]
+
+
+def workout_gen():
+    push = random.choice(push_exercise)
+    pull = random.choice(pull_exercise)
+    squat = random.choice(squat_exercise)
+    lunge = random.choice(lunge_exercise)
+    carry = random.choice(carry_exercise)
+    hinge = random.choice(hinge_exercise)
+	
+    workout = {
+        "Push exercise": push,
+        "Pull exercise": pull,
+        "Squat exercise": squat,
+        "Lunge exercise": lunge,
+        "Carry exercise": carry,
+        "Hinge exercise": hinge
+    }
+
+    return workout
+
+if __name__ == "__main__":
+    wokrout = workout_gen()
+    print(workout)
+
+
